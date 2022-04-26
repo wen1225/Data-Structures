2d519dc7d030a652c3f9a4c45dee4b4004ab658b
Wen Yao Ho <wenyao@Wens-MacBook-Pro.local>
2022-04-26 00:13:48 -0700

    implemented insert at the beginning and insert in the middle function

--- LinkedLists/linked_list_basic.cpp
+++ LinkedLists/linked_list_basic.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
+
 using namespace std;
-class LinkedList {
-    private:
-        struct Node {
-            int val;
-            Node* next;
-        };
-        Node* head;
-    public:
-        LinkedList() {head = nullptr;}
-        void printNode() const;                                     
-        void insertNode(int);
-        void deleteNode(int);
+
+class LinkedList
+{
+private:
+    struct Node
+    {
+        int val;
+        Node *next;
+    };
+    Node *head;
+
+public:
+    LinkedList() { head = nullptr; }
+    void printNode() const;
+    void insertNodeBegin(int);
+    void insertNodeMiddle(int);
+    void insertNodeEnd(int);
+    void deleteNode(int);
 };
 
-void LinkedList::printNode() const {
+void LinkedList::printNode() const
+{
     if (head == nullptr)
-     return;
-    Node* iter = head;
-    while (iter->next != nullptr) {
+        return;
+    Node *iter = head;
+    while (iter->next != nullptr)
+    {
         cout << iter->val << " -> ";
         iter = iter->next;
     }
@@ -26,44 +35,98 @@ void LinkedList::printNode() const {
     cout << endl;
 }
 
-void LinkedList::insertNode(int val) {
+void LinkedList::insertNodeBegin(int val)
+{
+    Node *nodePtr = new Node;
+    nodePtr->val = val;
+    nodePtr->next = nullptr;
+    if (head == nullptr)
+    {
+        head = nodePtr;
+    }
+    else
+    {
+        nodePtr->next = head;
+        head = nodePtr;
+    }
+}
+
+void LinkedList::insertNodeMiddle(int val) {
+    //1. Find the length of the linked list
+    //2. Divide the length by two, save it to var
+    //3. Re-iterate the list again until we hit the nth node that is one less than var
+    //4. Insert the new node next to the nth node one less than var
+
+    //Edge cases:
+    // How does it handle a list w/ a single node
+    Node* iter = head;
+    int listLength = 0;
+    while (iter != nullptr) 
+    {
+        listLength++;
+        iter = iter->next;
+    }
+    listLength = listLength / 2;
+    iter = head;
+    while (listLength > 1)
+    {
+        iter = iter->next;
+        listLength--;
+    }
+    Node *nodePtr = new Node;
+    nodePtr->val = val;
+    nodePtr->next = iter->next;
+    iter->next = nodePtr;
+}
+
+void LinkedList::insertNodeEnd(int val)
+{
     /**
      * Case 1: if head is empty, let head point to new node.
      * Case 2: if head is not empty, iterate till the last node and use last node's next to point to new node.
      */
-    Node* nodePtr = new Node;
+    Node *nodePtr = new Node;
     nodePtr->val = val;
     nodePtr->next = nullptr;
-    if (head == nullptr) {
+    if (head == nullptr)
+    {
         head = nodePtr;
     }
-    else {
-        Node* iter = head;
-        while (iter->next != nullptr) {
+    else
+    {
+        Node *iter = head;
+        while (iter->next != nullptr)
+        {
             iter = iter->next;
         }
         iter->next = nodePtr;
     }
 }
 
-void LinkedList::deleteNode(int val) {
+void LinkedList::deleteNode(int val)
+{
     /**
      * Case 1: if head is null/empty, return.
      * Case 2: if head is target, use dummy pointer to save remaining list, then delete head and reassign head w/ remaining list stored in dummy pointer.
      * Case 3: if target in middle, iterate the list with two ptr, one behind the other. The prev ptr will link the list after deletion.
+     * Case 4: if target is not found, then return a print statement saying target not found
      */
-    if (!head) 
+    if (!head)
         return;
-    else if (head->val == val) {
-        Node* iter = head->next;
+    else if (head->val == val)
+    {
+        Node *iter = head->next;
         delete head;
         head = iter;
     }
-    else {
-        Node* iter = head;
-        Node* prevNode = head;
-        while (iter != nullptr) {
-            if (iter->val == val) {
+    else
+    {
+        Node *iter = head;
+        Node *prevNode = head;
+        while (iter != nullptr)
+        {
+            if (iter->val == val)
+            {
                 prevNode->next = iter->next;
                 delete iter;
                 return;
@@ -71,35 +134,30 @@ void LinkedList::deleteNode(int val) {
             prevNode = iter;
             iter = iter->next;
         }
+        cout << "Node not found!\n";
     }
 }
 
-int main() {
-    string x;
+int main()
+{
     LinkedList list;
 
-    list.insertNode(1);
+    list.insertNodeEnd(1);
     list.printNode();
 
-    list.insertNode(4);
+    list.insertNodeMiddle(2);
     list.printNode();
 
-    list.insertNode(6);
+    list.insertNodeEnd(4);
     list.printNode();
 
-    list.insertNode(7);
+    list.insertNodeEnd(6);
     list.printNode();
 
-    list.deleteNode(1);
+    list.insertNodeEnd(7);
     list.printNode();
 
-    list.deleteNode(6);
-    list.printNode();
-
-    list.deleteNode(7);
-    list.printNode();
-
-    list.deleteNode(4);
+    list.insertNodeMiddle(8);
     list.printNode();
 
     return 0;
